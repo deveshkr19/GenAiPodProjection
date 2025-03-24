aa28b44b78d15f5093e93424606d176c88862012
deveshkr19 <deveshkr19@gmail.com>
2025-03-24 08:16:25 +0100

    Imporoved predcition

--- app/utils/model_training.py
+++ app/utils/model_training.py
@@ -8,16 +8,17 @@ def train_models(df):
     mem_target = "Memory_Load"
 
     X = df[features]
-    y_cpu = df[cpu_target]
-    y_mem = df[mem_target]
+    y = df[[cpu_target, mem_target]]
 
-    X_train, X_test, y_cpu_train, y_cpu_test = train_test_split(X, y_cpu, test_size=0.2, random_state=42)
-    _, _, y_mem_train, y_mem_test = train_test_split(X, y_mem, test_size=0.2, random_state=42)
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-    cpu_model = LinearRegression().fit(X_train, y_cpu_train)
-    mem_model = LinearRegression().fit(X_train, y_mem_train)
+    cpu_model = LinearRegression().fit(X_train, y_train[cpu_target])
+    mem_model = LinearRegression().fit(X_train, y_train[mem_target])
 
-    cpu_r2 = r2_score(y_cpu_test, cpu_model.predict(X_test))
-    mem_r2 = r2_score(y_mem_test, mem_model.predict(X_test))
+    cpu_r2 = r2_score(y_test[cpu_target], cpu_model.predict(X_test))
+    mem_r2 = r2_score(y_test[mem_target], mem_model.predict(X_test))
+
+    print("CPU Model Coefficients:", cpu_model.coef_)
+    print("MEM Model Coefficients:", mem_model.coef_)
 
     return cpu_model, mem_model, cpu_r2, mem_r2
